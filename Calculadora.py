c92e5bc22f999338ca520fdedea6b57fc60e2b7a
Viviane Andrade <vivianeandrade@MacBook-Pro-de-Viviane.local>
2021-02-11 08:37:13 -0300

    Validacao de even e odd

--- Calculadora.py
+++ Calculadora.py
@@ -2,20 +2,31 @@
 #numero 1
 #numero 2
 #tipo da operação (+,-,*,/)
-
+# A função mod calcula o resto da divisão. Por isso ao colocar % 2 e mod > 0, está validando se o resto é igual a zero ou não para saber se é even or odd).
 
 import sys
 try:
     numero1 = int(input("Insira o primeiro número: "))
+    mod = numero1 % 2
+    if mod > 0:
+        print("This is an odd number.")
+    else:
+        print("This is an even number.")
     numero2 = int(input("Insira o segundo número: "))
+    mod = numero2 % 2
+    if mod > 0:
+        print("This is an odd number.")
+    else:
+        print("This is an even number.")
     operacao = input("Selecione a operação ")
 
 except ValueError:
     print("Digite somente números!")
     sys.exit(1)
 
+
 if operacao == "+":
-   print("Resultado da soma:", numero1, "+", numero2, "=", numero1 + numero2)
+    print("Resultado da soma:", numero1, "+", numero2, "=", numero1 + numero2)
 elif operacao == "-":
     print("Resultado da subtração:", numero1, "-", numero2, "=", numero1 - numero2)
 elif operacao == "*":
